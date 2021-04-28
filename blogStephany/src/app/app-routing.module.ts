6f82922bdc61dda9184f3969cf36c1801de75aaa
Stephanymacedo <stephanymaceo@outlook.pt>
2021-04-28 09:08:43 -0300

    criando rotas e adcionando imagens

--- blogStephany/src/app/app-routing.module.ts
+++ blogStephany/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { EntrarComponent } from './entrar/entrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{path:'', redirectTo:'entrar', pathMatch:'full'},
+
+{path:'entrar', component:EntrarComponent},
+{path:'cadastrar', component:CadastroComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
