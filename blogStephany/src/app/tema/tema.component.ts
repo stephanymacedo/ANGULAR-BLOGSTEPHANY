5bef7f45f3bd1d92ee7052f113da1d9f0951055c
Stephanymacedo <stephanymaceo@outlook.pt>
2021-05-06 13:56:29 -0300

    criação do get post e temas

--- blogStephany/src/app/tema/tema.component.ts
+++ blogStephany/src/app/tema/tema.component.ts
@@ -1,22 +1,45 @@
 import { environment } from 'src/environments/environment.prod';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
-  templateUrl: './tema.component.html',
+  templateUrl:'./tema.component.html',
   styleUrls: ['./tema.component.css']
 })
 export class TemaComponent implements OnInit {
 
+  tema:Tema = new Tema()
+  listaTemas: Tema[]
+
   constructor(
-    private router:Router
+    private router:Router,
+    private temaService:TemaService
   ) { }
 
   ngOnInit(){
       if(environment.token ==''){
         this.router.navigate(['/entrar'])
       }
+
+      this.findAllTemas()
+  }
+
+  findAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[])=>{
+      this.listaTemas=resp
+    })
+  }
+
+  cadastrar(){
+    this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
+      this.tema=resp
+      alert('Tema cadastrado com sucesso!')
+      this.findAllTemas()
+      this.tema= new Tema()
+    })
   }
 
 }
