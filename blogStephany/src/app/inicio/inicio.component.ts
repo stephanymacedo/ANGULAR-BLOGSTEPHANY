aaf8737faab4a0e089b49a5348b4af20cbc02b87
Stephanymacedo <stephanymaceo@outlook.pt>
2021-05-12 11:25:22 -0300

    pesquisas implementadas

--- blogStephany/src/app/inicio/inicio.component.ts
+++ blogStephany/src/app/inicio/inicio.component.ts
@@ -1,3 +1,4 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
@@ -19,10 +20,13 @@ export class InicioComponent implements OnInit {
 
   postagem: Postagem = new Postagem()
   listaPostagens: Postagem[]
+  tituloPost: string
+
 
   tema: Tema = new Tema()
   listaTemas: Tema[]
   idTema: number
+  nomeTema: string
 
   user: User = new User()
   idUser = environment.id
@@ -86,6 +90,27 @@ export class InicioComponent implements OnInit {
       this.postagem = new Postagem()
       this.getAllPostagens()
     })
+  }
+
+  findByTituloPostagem(){
+
+    if(this.tituloPost==''){
+      this.getAllPostagens()
+    }else{
+      this.postagemService.getByTituloPostagem(this.tituloPost).subscribe((resp: Postagem[])=>{
+        this.listaPostagens= resp
+      })
+    }
+  }
+  
+  findByNomeTema(){
+    if(this.nomeTema==''){
+      this.getAllTemas()
+    }else{
+      this.temaService.getByNomeTema(this.nomeTema).subscribe((resp: Tema[])=>{
+        this.listaTemas=resp
+      })
+    }
   }
 
 }
--- blogStephany/src/app/service/tema.service.ts
+++ blogStephany/src/app/service/tema.service.ts
@@ -23,6 +23,10 @@ export class TemaService {
     return this.http.get<Tema>(`http://localhost:8080/temas/${id}`,this.token)
   }
 
+  getByNomeTema(nome: string) : Observable<Tema[]>{
+    return this.http.get<Tema[]>(`http://localhost:8080/temas/nome/${nome}` ,this.token)
+  }
+
   postTema(tema: Tema):Observable<Tema>{
     return this.http.post<Tema>('http://localhost:8080/temas',tema, this.token)
   }
@@ -34,6 +38,8 @@ export class TemaService {
   deleteTema(id:number){
     return this.http.delete(`http://localhost:8080/temas/${id}`,this.token)
   }
+
+
   
   
 }
